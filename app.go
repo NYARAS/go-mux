1eb224db48be97a635e0343e0b03cbb9dc143a07
Calvine Otieno <nyarangaotieno@gmail.com>
2022-02-25 16:34:20 +0300

    chore: add delete product handler
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- app.go
+++ app.go
@@ -133,3 +133,20 @@ func (a *App) updateProduct(w http.ResponseWriter, r *http.Request) {
 
 	respondWithJSON(w, http.StatusOK, p)
 }
+
+func (a *App) deleteProduct(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid product ID")
+		return
+	}
+
+	p := Product{ID: id}
+	if err := p.deleteProduct(a.DB); err != nil {
+		respondWithError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, map[string]string{"result": "success"})
+}
