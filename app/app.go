d83748dad50d3856202d7b70f8c6149a633c0eca
Calvine Otieno <nyarangaotieno@gmail.com>
2022-02-27 17:48:23 +0300

    chore: add handlers to manage product data
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- app/app.go
+++ app/app.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/NYARAS/go-mux/config"
+	"github.com/NYARAS/go-mux/handler"
 	"github.com/NYARAS/go-mux/model"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
@@ -37,7 +38,10 @@ func (a *App) Initialize(config *config.Config) {
 }
 
 // Set all required routers
-func (a *App) setRouters() {}
+func (a *App) setRouters() {
+	a.Post("/products", a.CreateProduct)
+	a.Get("/products", a.GetAllProducts)
+}
 
 // Wrap the router for GET method
 func (a *App) Get(path string, f func(w http.ResponseWriter, r *http.Request)) {
@@ -57,6 +61,15 @@ func (a *App) Put(path string, f func(w http.ResponseWriter, r *http.Request)) {
 // Wrap the router for DELETE method
 func (a *App) Delete(path string, f func(w http.ResponseWriter, r *http.Request)) {
 	a.Router.HandleFunc(path, f).Methods("DELETE")
+}
+
+// Handlers to manage Prodcut Data
+func (a *App) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	handler.CreateProduct(a.DB, w, r)
+}
+
+func (a *App) GetAllProducts(w http.ResponseWriter, r *http.Request) {
+	handler.GetAllProducts(a.DB, w, r)
 }
 
 // Run the app on it's router
