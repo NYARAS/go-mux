d2d393c379e2b0a965b73c9f78358756b3022557
Calvine Otieno <nyarangaotieno@gmail.com>
2022-02-27 17:43:13 +0300

    ref: refactor app handlers and routes
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

---
+++ app/app.go
@@ -0,0 +1,65 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/NYARAS/go-mux/config"
+	"github.com/NYARAS/go-mux/model"
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+)
+
+// App has router and db instances
+type App struct {
+	Router *mux.Router
+	DB     *gorm.DB
+}
+
+// App initialize with predefined configuration
+func (a *App) Initialize(config *config.Config) {
+	dbURI := fmt.Sprintf("host=%v port=%v user=%v dbname=%v password=%v sslmode=disable",
+		config.DB.Host,
+		config.DB.Port,
+		config.DB.Username,
+		config.DB.Name,
+		config.DB.Password)
+
+	db, err := gorm.Open(config.DB.Dialect, dbURI)
+	if err != nil {
+		log.Fatal("Could not connect database")
+	}
+
+	a.DB = model.DBMigrate(db)
+	a.Router = mux.NewRouter()
+	a.setRouters()
+}
+
+// Set all required routers
+func (a *App) setRouters() {}
+
+// Wrap the router for GET method
+func (a *App) Get(path string, f func(w http.ResponseWriter, r *http.Request)) {
+	a.Router.HandleFunc(path, f).Methods("GET")
+}
+
+// Wrap the router for POST method
+func (a *App) Post(path string, f func(w http.ResponseWriter, r *http.Request)) {
+	a.Router.HandleFunc(path, f).Methods("POST")
+}
+
+// Wrap the router for PUT method
+func (a *App) Put(path string, f func(w http.ResponseWriter, r *http.Request)) {
+	a.Router.HandleFunc(path, f).Methods("PUT")
+}
+
+// Wrap the router for DELETE method
+func (a *App) Delete(path string, f func(w http.ResponseWriter, r *http.Request)) {
+	a.Router.HandleFunc(path, f).Methods("DELETE")
+}
+
+// Run the app on it's router
+func (a *App) Run(host string) {
+	log.Fatal(http.ListenAndServe(host, a.Router))
+}
