5854d6acd704a4030ab75c4eddb29173d67443e4
Calvine Otieno <nyarangaotieno@gmail.com>
2024-05-01 19:04:20 +0300

    chore: remove employee model
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- model/model.go
+++ model/model.go
@@ -1,17 +1,8 @@
 package model
 
 import (
-	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 )
-
-type Employee struct {
-	gorm.Model
-	Name   string `gorm:"unique" json:"name"`
-	City   string `json:"city"`
-	Age    int    `json:"age"`
-	Status bool   `json:"status"`
-}
 
 type SecretResponse struct {
 	AccessKey       string `json:"accesskey"`
